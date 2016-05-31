54bd787ac681751a6cf0394963a6e58c718e768c
Connor Lewis <connorlewis@utexas.edu>
2016-05-31 16:14:42 -0500

    finished moving stripe implementation

---
+++ MazamaCoffee/app/src/main/java/com/mazamacoffee/www/mazamacoffee/TokenList.java
@@ -0,0 +1,7 @@
+package com.mazamacoffee.www.mazamacoffee;
+
+import com.stripe.android.model.Token;
+
+public interface TokenList {
+    public void addToList(Token token);
+}
--- MazamaCoffee/app/src/main/java/com/mazamacoffee/www/mazamacoffee/activity/TransactionActivity.java
+++ MazamaCoffee/app/src/main/java/com/mazamacoffee/www/mazamacoffee/activity/TransactionActivity.java
@@ -1,33 +1,34 @@
 package com.mazamacoffee.www.mazamacoffee.activity;
 
-
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 
-
-import com.mazamacoffee.www.mazamacoffee.R;
-import com.mazamacoffee.www.mazamacoffee.TransactionForm;
-import com.stripe.android.*;
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
-import com.stripe.android.Stripe.*;
 import com.stripe.android.model.Token;
-import com.stripe.exception.AuthenticationException;
+import com.mazamacoffee.www.mazamacoffee.TransactionForm;
+import com.mazamacoffee.www.mazamacoffee.R;
+import com.mazamacoffee.www.mazamacoffee.TokenList;
+import com.mazamacoffee.www.mazamacoffee.dialog.ErrorDialogFragment;
+import com.mazamacoffee.www.mazamacoffee.dialog.ProgressDialogFragment;
+
+
+import com.stripe.android.model.Token;
 
 public class TransactionActivity extends FragmentActivity {
-    private EditText cardNumber;
-    private EditText month;
-    private EditText year;
-    private EditText cvv;
+
+    public static final String PUBLISHABLE_KEY = "pk_test_irEAw0mlXyAIwo6XFQRlBpOi";
+
+    private ProgressDialogFragment progressFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+        progressFragment = ProgressDialogFragment.newInstance(R.string.progressMessage);
     }
 
     public void transactionSubmit(TransactionForm form) {
@@ -38,24 +39,47 @@ public class TransactionActivity extends FragmentActivity {
                 form.getExpYear(),
                 form.getCvv());
         Boolean validate = card.validateCard();
-        if(validate){
-
+        boolean validation = card.validateCard();
+        if (validation) {
+            startProgress();
+            new Stripe().createToken(
+                    card,
+                    PUBLISHABLE_KEY,
+                    new TokenCallback() {
+                        public void onSuccess(Token token) {
+                            getTokenList().addToList(token);
+                            finishProgress();
+                        }
+                        public void onError(Exception error) {
+                            handleError(error.getLocalizedMessage());
+                            finishProgress();
+                        }
+                    });
+        } else if (!card.validateNumber()) {
+            handleError("The card number that you entered is invalid");
+        } else if (!card.validateExpiryDate()) {
+            handleError("The expiration date that you entered is invalid");
+        } else if (!card.validateCVC()) {
+            handleError("The CVC code that you entered is invalid");
+        } else {
+            handleError("The card details that you entered are invalid");
         }
-        /*Stripe stripe = new Stripe("pk_test_irEAw0mlXyAIwo6XFQRlBpOi");
-        stripe.createToken(
-                card,
-                new TokenCallback() {
-                    public void onSuccess(Token token) {
-                        // Send token to your server
-                    }
-                    public void onError(Exception error) {
-                        // Show localized error message
-                        Toast.makeText(getContext(),
-                                error.getLocalizedString(getContext()),
-                                Toast.LENGTH_LONG
-                        ).show();
-                    }
-                }
-        );*/
+    }
+
+    private void startProgress() {
+        progressFragment.show(getSupportFragmentManager(), "progress");
+    }
+
+    private void finishProgress() {
+        progressFragment.dismiss();
+    }
+
+    private void handleError(String error) {
+        DialogFragment fragment = ErrorDialogFragment.newInstance(R.string.validationErrors, error);
+        fragment.show(getSupportFragmentManager(), "error");
+    }
+
+    private TokenList getTokenList() {
+        return (TokenList)(getSupportFragmentManager().findFragmentById(R.id.token_list));
     }
 }
